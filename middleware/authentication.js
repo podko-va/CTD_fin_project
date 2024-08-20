75182563dd571bbc2527e7375382a4471c3220c1
Kate <mykateWgmail.com>
2024-08-20 14:52:31 +0300

    A bit of logging for debugging_1

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,6 @@ const auth = (req,res,next) =>{
     //check header
     const authHeader = req.headers.authorization
     if (!authHeader || !authHeader.startsWith('Bearer')){
-        console.log('auth_middleware_Bearer.js')
         throw new UnauthenticatedError('Authentification invalid')
     }
     const token = authHeader.split(' ')[1]
@@ -17,7 +16,11 @@ const auth = (req,res,next) =>{
     req.user = {userId:payload.userId, name:payload.name}
     next()
  } catch (error) {
-    console.log('auth_middleware.js')
+    console.log(error)
+    console.log(token)
+    console.log(req.user)
+    
+    
     throw new UnauthenticatedError('Authentification invalid')  
  }
 }

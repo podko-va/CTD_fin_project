2195d7fbddff1cdfef2cde07d9253599f01a19a3
Kate <mykateWgmail.com>
2024-08-20 21:55:46 +0300

    swagger

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 const authenticateUser = require('./middleware/authentication');
@@ -34,9 +39,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/',(req,res)=>{
-  res.send('JOBS API')
-})
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // extra packages
 
 // routes

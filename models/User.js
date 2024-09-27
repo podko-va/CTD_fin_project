5f45afc69e6ba405c5cc3d7abc19bea026406e70
Kate <mykateWgmail.com>
2024-09-27 13:01:41 -0700

    added schema

--- models/User.js
+++ models/User.js
@@ -2,26 +2,41 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required : [true, 'please provide a name'],
+        required: [true, 'Please provide a name'],
         maxlength: 50,
         minlength: 3,
     },
     email: {
         type: String,
-        required : [true, 'please provide email'],
+        required: [true, 'Please provide an email'],
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'please provide a valid email'],
+        'Please provide a valid email'],
         unique: true,
     },
     password: {
         type: String,
-        required : [true, 'please provide password'],
+        required: [true, 'Please provide a password'],
         minlength: 6,
+    },
+    isPsychologist: {
+        type: Boolean,
+        default: false, // true for psychologists
+    },
+    timezone: {
+        type: String,
+        required: [true, 'Please provide your timezone'],
+        enum: ['PDT', 'CET', 'EDT', 'UTC', 'GMT', 'EST', 'IST', 'CST'], // example timezone options
+    },
+    gender: {
+        type: String,
+        required: [true, 'Please provide gender'],
+        enum: ['female', 'male'],
     }
-})
+});
 
 userSchema.pre('save', async function()
 {
@@ -38,4 +53,4 @@ userSchema.methods.comparePassword = async function (canditatePassword) {
     return isMatch
   }
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User_FP', userSchema)
